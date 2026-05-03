f1498b6c9cb77def67b8ba252e091a46ce63d5c3
허세진 <heosejin222@gmail.com>
2026-05-03 23:17:40 +0900

    Initial commit

---
+++ src/test/java/com/liveklass/testa/TestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveklass.testa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
